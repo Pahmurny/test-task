b17dc3e85401f250cdefc6d2a8871c957e289251
Viveka <viveka@careerlark.com>
2018-04-03 17:37:57 -0700

    1st round of updates

--- src/components/Icons/CheckMarkIcon.js
+++ src/components/Icons/CheckMarkIcon.js
@@ -7,8 +7,8 @@ const CheckMarkIcon = styled.div`
     background-image: url(${ico});
     background-position: center center;
     background-repeat: no-repeat;
-    width: 14px;
-    height: 17px;
+    width: 15px;
+    height: 18px;
 `
 
 
