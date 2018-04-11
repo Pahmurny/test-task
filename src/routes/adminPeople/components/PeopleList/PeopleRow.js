d45d3bf94ff324123aaf266e01e0a8557b8eba9e
Viveka <viveka@careerlark.com>
2018-04-11 13:21:30 -0700

    CSS edits: 4/11/18 (1)

--- src/routes/adminPeople/components/PeopleList/PeopleRow.js
+++ src/routes/adminPeople/components/PeopleList/PeopleRow.js
@@ -5,7 +5,7 @@ const PeopleRow = styled.div`
     display: flex;
     align-items: center;
     border-bottom: ${props => props.last ? 0 : '1px'} solid #9f9ba2;
-    min-height: 47px;
+    min-height: 48px;
     font-family: LatoRegular,sans-serif;
     font-size: 14px;
     color: #23182D;
