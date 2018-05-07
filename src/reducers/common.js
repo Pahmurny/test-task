9fdf39d45233f8f5569e3841b2c0bd714b476ca1
Denis Rudov <denis.rudov@gmail.com>
2018-05-07 20:21:50 +0300

    remove fake data

--- src/reducers/common.js
+++ src/reducers/common.js
@@ -10,32 +10,6 @@ const initialState = {
     name: 'John Doe',
     companyId: 1,
   },
-  showUserProfile: {
-    id: 2,
-    name: 'Jena Oberbrunner',
-    phone_number: '650-555-8014',
-    preferred_name: 'Jena',
-    job_title: 'Head of Biz Dev',
-    email: 'jena@fake.com',
-    first_name: 'Jena',
-    last_name: 'Oberbrunner',
-    image: 'https://s3.amazonaws.com/uifaces/faces/twitter/frankiefreesbie/128.jpg',
-    title: 'CEO',
-    managerId: 8,
-    team_tags: [
-      {
-        label: 'Engineering',
-        value: 'Engineering',
-      },
-      {
-        label: 'Project Babel',
-        value: 'Project Babel',
-      },
-    ],
-    active: true,
-    isAdmin: true,
-    companyId: 1,
-  },
 }
 
 
