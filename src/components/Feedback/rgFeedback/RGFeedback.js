86ced8bf7242c9e9b66552b1a6620a88a2832eec
Denis Rudov <denis.rudov@gmail.com>
2018-05-15 19:25:43 +0300

    feedback module header render

--- src/components/Feedback/rgFeedback/RGFeedback.js
+++ src/components/Feedback/rgFeedback/RGFeedback.js
@@ -3,11 +3,14 @@ import FromToFeedback from 'components/Feedback/FromToFeedback'
 import { IsDev } from 'helpers/dev'
 import './rgfeedback.scss'
 import AboutBlock from 'components/Feedback/AboutBlock'
+import { feedbackHeader } from 'components/FeedbacksList/FeedbacksList'
+import { MODULE_VIEW_COMPANY } from 'routes/feedback/feedbackTypes'
 
 
 const RGFeedback = (props) => <FromToFeedback
   {...props}
   className={'received-given'}
+  render={props=> feedbackHeader[MODULE_VIEW_COMPANY](props)}
 >
   {props => <div className={'received-given__content'}>
     {(props.question || IsDev()) && <div className="received-given__question">{props.question || 'Request: How’s my transition as a part-time advisor?'}</div>}
